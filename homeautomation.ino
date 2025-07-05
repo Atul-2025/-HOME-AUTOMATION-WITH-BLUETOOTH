23554804b4a1f8a4feb3fcadaab7c96494006b3e
Atul-2025 <maphch9999@gmail.com>
2025-07-05 21:27:04 +0530

    Update homeautomation.ino

--- homeautomation.ino
+++ homeautomation.ino
@@ -1,55 +1,61 @@
-#define BLYNK_TEMPLATE_ID "TMPL3sBj1Zbsp"
-#define BLYNK_TEMPLATE_NAME "Home automation"
-#define BLYNK_AUTH_TOKEN "4m0ffdE4XAeOVlLhmHWMftniTONpLj7Q"
+#include <SoftwareSerial.h>               // Include SoftwareSerial library to allow serial communication on other digital pins
 
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
+SoftwareSerial mySerial(6, 7);            // Create a SoftwareSerial object named 'mySerial' using pin 6 as RX and pin 7 as TX (used for Bluetooth like HC-05)
 
-char auth[] = "4m0ffdE4XAeOVlLhmHWMftniTONpLj7Q";
-char ssid[] = "Redmi 9A";//Enter your WIFI name
-char pass[] = "123456abc";//En
+void setup()
+{
+  Serial.begin(9600);                     // Initialize serial communication with the computer at 9600 baud rate
+  mySerial.begin(9600);                   // Initialize software serial communication (e.g., with HC-05 Bluetooth module) at 9600 baud rate
 
-const int relayPins[] = {D0, D1, D2, D3};
+  pinMode(12, OUTPUT);                    // Set digital pin 12 as output (e.g., to control a device/LED)
+  pinMode(11, OUTPUT);                    // Set digital pin 11 as output
+  pinMode(10, OUTPUT);                    // Set digital pin 10 as output
+  pinMode(9, OUTPUT);                     // Set digital pin 9 as output
 
-void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
+  digitalWrite(12, HIGH);                 // Turn off device connected to pin 12 (assuming active LOW configuration)
+  digitalWrite(11, HIGH);                 // Turn off device on pin 11
+  digitalWrite(10, HIGH);                 // Turn off device on pin 10
+  digitalWrite(9, HIGH);                  // Turn off device on pin 9
+}
+
+void loop()
+{
+  if (mySerial.available() > 0)           // Check if data is available from the Bluetooth module
+  {
+    char data = mySerial.read();          // Read one character from the Bluetooth serial
+    Serial.println(data);                 // Print the received character to the serial monitor for debugging
+
+    if (data == 'B')                      // If the received character is 'B'
+    {
+      digitalWrite(12, HIGH);            // Turn OFF the device on pin 12 (HIGH = off in active LOW logic)
+    } 
+    else if (data == 'A')                 // If received 'A'
+    {
+      digitalWrite(12, LOW);             // Turn ON the device on pin 12
+    }
+    else if (data == 'D')                 // If received 'D'
+    {
+      digitalWrite(11, HIGH);            // Turn OFF device on pin 11
+    }
+    else if (data == 'C')                 // If received 'C'
+    {
+      digitalWrite(11, LOW);             // Turn ON device on pin 11
+    }
+    else if (data == 'F')                 // If received 'F'
+    {
+      digitalWrite(10, HIGH);            // Turn OFF device on pin 10
+    }
+    else if (data == 'E')                 // If received 'E'
+    {
+      digitalWrite(10, LOW);             // Turn ON device on pin 10
+    }
+    else if (data == 'H')                 // If received 'H'
+    {
+      digitalWrite(9, HIGH);             // Turn OFF device on pin 9
+    }
+    else if (data == 'G')                 // If received 'G'
+    {
+      digitalWrite(9, LOW);              // Turn ON device on pin 9
+    }
   }
-  Serial.println("Connected to WiFi");
-  Blynk.begin(auth, ssid, pass);
-  for (int i = 0; i < 4; i++) {
-    pinMode(relayPins[i], OUTPUT);
-    digitalWrite(relayPins[i], HIGH); // Initialize relays as OFF
-  }
-}
-
-BLYNK_WRITE(V1) {
-  int value = param.asInt();
-  digitalWrite(D0, !value);
-  Serial.println(value ? "Relay 1 ON" : "Relay 1 OFF");
-}
-
-BLYNK_WRITE(V2) {
-  int value = param.asInt();
-  digitalWrite(D1, !value);
-  Serial.println(value ? "Relay 2 ON" : "Relay 2 OFF");
-}
-
-BLYNK_WRITE(V3) {
-  int value = param.asInt();
-  digitalWrite(D2, !value);
-  Serial.println(value ? "Relay 3 ON" : "Relay 3 OFF");
-}
-
-BLYNK_WRITE(V4) {
-  int value = param.asInt();
-  digitalWrite(D3, !value);
-  Serial.println(value ? "Relay 4 ON" : "Relay 4 OFF");
-}
-
-void loop() {
-  Blynk.run();
 }
